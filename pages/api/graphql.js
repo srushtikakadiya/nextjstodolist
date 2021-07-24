e31870c6f572bdf7ccc2ad3450049d3358637eec
srushti kakadiya <srushtikakadiya2701@gmail.com>
2021-07-24 09:32:00 +0530

    first commit

---
+++ pages/api/graphql.js
@@ -0,0 +1,56 @@
+// import {ApolloServer , gql } from 'apollo-server-micro'
+
+// const schema = gql`
+// type Query{
+//     hello:String!
+// }
+// `;
+
+// const resolvers = {
+//     Query: {
+//         hello: (_parent, _args, _context) => "World!"
+//     }
+// }
+
+// const apolloServer = new ApolloServer({
+//   typeDefs: schema,
+//   resolvers,
+//   context: () => {
+//     return {}
+//   }
+// })
+
+// const handler = apolloServer.createHandler({ path: "/api/graphql"});
+// export const config = {
+//   api: {
+//     bodyParser: false
+//   }
+// };
+
+// export default handler;
+    
+
+import { ApolloServer, gql } from 'apollo-server-micro'
+
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    sayHello(parent, args, context) {
+      return 'Hello World!'
+    },
+  },
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+export default apolloServer.createHandler({ path: '/api/graphql' })
